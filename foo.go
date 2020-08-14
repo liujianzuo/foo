c6206e9615b9988cdae2b185d3137201bd1a3231
刘建佐 <liujianzuo@gmail.com>
2020-08-14 10:21:54 +0800

    增加新库

--- foo.go
+++ foo.go
@@ -3,7 +3,10 @@ package foo
 import (
 	"fmt"
 	"github.com/Jeffail/tunny"
+	"runtime"
 )
 func Greet(name string) string {
+	numCPUs := runtime.NumCPU()
+	fmt.Println("numCPUs: ", numCPUs)
 	return fmt.Sprintf("%s, 你好！ Version 3.0.0", name)
 }
