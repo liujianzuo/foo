8263c7a1c7dc273daf4b4881a6f792a56e436f34
刘建佐 <liujianzuo@gmail.com>
2020-08-14 10:25:53 +0800

    增加新库

--- foo.go
+++ foo.go
@@ -8,5 +8,14 @@ import (
 func Greet(name string) string {
 	numCPUs := runtime.NumCPU()
 	fmt.Println("numCPUs: ", numCPUs)
+	pool := tunny.NewFunc(numCPUs, func(payload interface{}) interface{} {
+		var result []byte
+
+		// TODO: Something CPU heavy with payload
+
+		return result
+	})
+	defer pool.Close()
+	fmt.Println("sdfsf: ", pool)
 	return fmt.Sprintf("%s, 你好！ Version 3.0.0", name)
 }
